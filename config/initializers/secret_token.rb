e6bb3e8f3736dd5cc99bc01083454f52f6d7dc00
Sebastian Fröstl <sebastian@froestl.com>
2012-05-04 14:25:57 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onespark::Application.config.secret_token = '0f01ee50b524f4847254dc07eb3338b235d40491ab03f5e0274c3052159c2e1f17e2f0dd719597d02605d524d860a3173982a9a51937054b598843a77ff5fc06'
