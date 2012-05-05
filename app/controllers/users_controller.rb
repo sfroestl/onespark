a0026137e48ed1ef7ed94a6691ecc3479092a0f4
Sebastian Fröstl <sebastian@froestl.com>
2012-05-05 21:47:54 +0200

    User signup ready, also database ready for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create
-     @user = User.new(params[:user])
+    @user = User.new(params[:user])
+        if @user.save
+          redirect_to @user
+        else
+          render 'new'
+        end
   end
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,11 @@
 Onespark::Application.configure do
+  
+  # Speed up tests by lowering BCrypt's cost function.
+    require 'bcrypt'
+    silence_warnings do
+      BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+    end
+  
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
