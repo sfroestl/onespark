06e3dccd587aeed54921d8e67e1b950007d2faf4
Sebastian Fröstl <sebastian@froestl.com>
2012-05-06 17:01:18 +0200

    changed singup form_for to form_tag because it's not related to a model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
   def new
-    end
+  end
 
-    def create
-      user = User.find_by_email(params[:session][:email])
-      if user && user.authenticate(params[:session][:password])
-        sign_in user
-        redirect_to user
-      else
-        flash.now[:error] = 'Invalid email/password combination'
-        render 'new'
-      end
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      sign_in user
+      redirect_to user
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
     end
-    
-    def destroy
-      sign_out
-      redirect_to root_path
-    end
+  end
+  
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
 end
