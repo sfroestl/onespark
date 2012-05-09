36597242990dddc98bada569d8dd375e888aa03b
Sebastian Fröstl <sebastian@froestl.com>
2012-05-09 16:37:55 +0200

    projects create, delete, edit, show ready

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,19 +1,63 @@
 class ProjectsController < ApplicationController
+before_filter :get_all_projects
 
 def new
-  @projects = Project.all
   @project = Project.new
 end
 
 def index
-  @projects = Project.all
 end
 
 def show
-  @projects = Project.all
-  @project = Project.find(params[:id])
+  @project = find_project(params[:id])
 end
 
+def create
+    @project = Project.new(params[:project])
+    if @project.save
+      flash.now[:success] = "Project created!"
+      get_all_projects
+      render 'index'
+    else
+      flash.now[:error] = "Error! Project not created!"
+      render 'new'
+    end
+end
 
+def destroy
+  if project = find_project(params[:id])
+     project.destroy
+     flash.now[:success] = "Your project has been deleted."
+     get_all_projects
+     render 'index'
+  else
+    flash.now[:error] = "No such project!"
+    render 'index'
+   end
+ end
+ 
+def edit
+  @project = find_project(params[:id])
+end 
 
+def update
+    @project = find_project(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:success] = "Project updated"
+      get_all_projects
+      render 'index'
+    else
+      render 'edit'
+  end
+end
+
+private
+
+  def get_all_projects
+    @projects = Project.all
+  end
+  
+  def find_project(id)
+    Project.find(params[:id])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
       else
         render 'new'
       end
-    end
+  end
     
   def edit
       @user = User.find(params[:id])
