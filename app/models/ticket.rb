812012ce24d0d35bcc674b080589f767cbc54a72
Sebastian Fröstl <sebastian@froestl.com>
2012-05-10 16:19:12 +0200

    final status after completing tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,16 +5,17 @@ class Ticket < ActiveRecord::Base
   validates :title, presence:true, length: { minimum: 4 }
   validates :project_id, presence:true
 
-  validate :due_date_not_in_past_but_can_be_empty
-
-  def due_date_not_in_past_but_can_be_empty
-    if self.due_date.nil?
-      true
-    elsif  self.due_date < DateTime.current
-      errors.add(:due_date, 'You can\'t complete tasks in the past!')
-    end
-  end
+  validate :due_date_not_in_past
   
   # Default order descending depening on due_date  
     default_scope order: 'tickets.due_date ASC'
+
+    def due_date_not_in_past
+       if self.due_date.nil?
+         true
+       elsif  self.due_date < DateTime.current
+         errors.add(:due_date, 'You can\'t complete tasks in the past!')
+       end
+     end
+
 end
