fc9e66313172ddc35f83110af449cf4cdda93a81
Sebastian Fröstl <sebastian@froestl.com>
2012-07-09 01:05:27 +0200

    github tool ready

---
+++ lib/tools/github/resources/commit.rb
@@ -0,0 +1,7 @@
+# See GitHubV3API documentation in lib/github_v3_api.rb
+class GitHubApi
+  # Represents a single GitHub Issue and provides access to its data attributes.
+  class Commit < Entity
+    attr_reader :url, :sha, :commit, :author, :message, :author, :parents
+  end  
+end
---
+++ lib/tools/github/resources/commits_api.rb
@@ -0,0 +1,41 @@
+# See GitHubApi documentation in lib/github_v3_api.rb
+class GitHubApi
+  # Provides access to the GitHub Issues API (http://developer.github.com/v3/commits/)
+  #
+  #
+  class CommitsAPI
+    # Typically not used directly. Use GitHubApi#commits instead.
+    #
+    # +connection+:: an instance of GitHubApi
+    def initialize(connection)
+      @connection = connection
+    end
+
+    # Returns an array of GitHubApi::Commit instances representing a
+    # user's commits or commits for a repo
+    def list(options, params={})
+      Rails.logger.info ">> Gihub commits API: #{options[:user]} #{options[:repo]}"
+      path = "/repos/#{options[:user]}/#{options[:repo]}/commits"
+
+      Rails.logger.info ">> Gihub commits API: path: #{path}"
+      @connection.get(path, params).map do |commit_data|        
+        GitHubApi::Commit.new(self, commit_data)
+      end
+    end
+
+    # Returns a GitHubApi::commit instance for the specified +user+,
+    # +repo_name+, and +sha+.
+    #
+    # +user+:: the string ID of the user, e.g. "octocat"
+    # +repo_name+:: the string ID of the repository, e.g. "hello-world"
+    # +id+:: the integer ID of the commit, e.g. 42
+    def get(user, repo_name, sha)
+      
+      commit_data = @connection.get("/repos/#{user}/#{repo_name}/commits/#{sha}", params)
+      GitHubApi::Commit.new_with_all_data(self, commit_data)
+    rescue RestClient::ResourceNotFound
+      raise NotFound, "The commit #{user}/#{repo_name}/commits/#{id} does not exist or is not visible to the user."
+    end
+
+  end
+end
