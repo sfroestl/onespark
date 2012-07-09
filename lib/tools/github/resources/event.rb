eb9cc05f5e8c46925bd7bef449d69da9993bff79
Sebastian Fröstl <sebastian@froestl.com>
2012-07-09 12:13:58 +0200

    GitHub Events extended

--- lib/tools/github/resources/event.rb
+++ lib/tools/github/resources/event.rb
@@ -2,6 +2,6 @@
 class GitHubApi
   # Represents a single GitHub Event and provides access to its data attributes.
   class Event < Entity
-    attr_reader :type, :public, :payload, :repo, :actor, :org, :created_at, :id
+    attr_reader :type, :public, :payload, :repo, :actor, :org, :created_at, :id, :comment
   end  
 end
