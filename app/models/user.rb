3dba4df3dffd52e040bfeef163d9dc4b89988e75
Sebastian Fröstl <sebastian@froestl.com>
2012-07-24 12:29:50 +0200

    account settings reworked

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :project_permissions, :through => :project_coworkers, :source => :project, dependent: :destroy 
   has_many :project_coworkers
 
-  has_many :friendships
+  has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships, conditions: "status = 'accepted'"
   has_many :requested_friends, through: :friendships, source: :friend, conditions: "status = 'requested'"
   has_many :pending_friends, through: :friendships, source: :friend, conditions: "status = 'pending'"
