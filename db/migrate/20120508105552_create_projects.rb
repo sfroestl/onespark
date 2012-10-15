b482420452abdc28fff548c10f40368f47bdc7a7
Sebastian Fröstl <sebastian@froestl.com>
2012-10-15 12:32:07 +0200

    refactoring DB migration

--- db/migrate/20120508105552_create_projects.rb
+++ db/migrate/20120508105552_create_projects.rb
@@ -4,6 +4,7 @@ class CreateProjects < ActiveRecord::Migration
       t.string :title
       t.text :desc
       t.datetime :due_date
+      t.integer :user_id
 
       t.timestamps
     end
--- db/migrate/20120616221808_rename_owner_id.rb
+++
@@ -1,6 +0,0 @@
-class RenameOwnerId < ActiveRecord::Migration
-  def up
-  	rename_column(:projects, :owner_id, :user_id)
-  	rename_column(:milestones, :owner_id, :user_id)
-  end
-end
